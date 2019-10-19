43e4c76faa3f4d7d335afab7f3ac2d09bbe34551
NatalNW <natanaelweslley1@gmail.com>
2019-10-19 12:38:44 -0300

    fisrt Kata (Square Every Digit)

---
+++ java-exercises/src/SquareDigit.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+/*
+ * Welcome. In this kata, you are asked to square every digit of a number.
+ * For example, if we run 9119 through the function, 811181 will come out, because 9² is 81 and 1² is 1.
+ * Note: The function accepts an integer and returns an integer
+ */
+public class SquareDigit {
+	private String str;
+	private int pow;
+	private StringBuilder result = new StringBuilder();
+	
+	public int squareDigits(Integer n) {
+		str = n.toString();
+	    
+		for(int i = 0; i < str.length(); i++) {
+			pow = Character.getNumericValue(str.charAt(i));
+			result.append(pow*pow);
+	    }
+		
+	    return Integer.parseInt(result.toString());
+	}
+}
