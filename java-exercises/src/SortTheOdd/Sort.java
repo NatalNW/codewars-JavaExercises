28c995c51f4289cf17410d3dcdec110d46d37a37
NatalNW <natanaelweslley1@gmail.com>
2019-10-21 16:41:50 -0300

    Adding finish date

--- java-exercises/src/SortTheOdd/Sort.java
+++ java-exercises/src/SortTheOdd/Sort.java
@@ -3,6 +3,8 @@ package SortTheOdd;
 /* You have an array of numbers.
  * Your task is to sort ascending odd numbers but even numbers must be on their places.
  * Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+ * 
+ * Finished 2019/10/21
  */
 public class Sort {
 	private static int min;
