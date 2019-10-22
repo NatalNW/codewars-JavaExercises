b7c7aebd656e19456325e14a62ba59904a8f84ab
NatalNW <natanaelweslley1@gmail.com>
2019-10-22 10:50:20 -0300

    Refactoring Dubstep exercise

---
+++ java-exercises/src/Dubstep/Main.java
@@ -0,0 +1,12 @@
+package Dubstep;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/* Dubstep */
+		System.out.println(Dubstep.SongDecoder("RWUBWUBWUBLWUB"));
+		System.out.println(Dubstep.SongDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"));
+		System.out.println(Dubstep.SongDecoder("WUBWUBABCWUB"));
+	}
+
+}
