3853747d69652ea1e96c7b5c97cfadbb092272ed
NatalNW <natanaelweslley1@gmail.com>
2019-10-23 12:23:37 -0300

    Sum of Digits / Digital Root

---
+++ java-exercises/src/SumOfDigits/Main.java
@@ -0,0 +1,10 @@
+package SumOfDigits;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/* Sum of Digits / Digital Root */
+		System.out.println(DigitalRoot.digitalRoot(16));
+		System.out.println(DigitalRoot.digitalRoot(456));
+	}
+}
