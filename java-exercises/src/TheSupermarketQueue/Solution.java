f24628d11fcbc18c5ce742db6f4d8f0ccc21c5d4
NatalNW <natanaelweslley1@gmail.com>
2019-11-14 17:09:15 -0300

    The Supermarket Queue

---
+++ java-exercises/src/TheSupermarketQueue/Solution.java
@@ -0,0 +1,30 @@
+package TheSupermarketQueue;
+
+import java.util.Arrays;
+
+/*
+ * 
+ * There is a queue for the self-checkout tills at the supermarket. 
+ * Your task is write a function to calculate the total time 
+ * required for all the customers to check out!
+ * 
+ * Finished 2019/11/14
+ */
+public class Solution {
+
+	public static int solveSuperMarketQueue(int[] customers, int n) {
+		if (customers.length == 0)
+			return 0;
+		else if (n == 1)
+			return Arrays.stream(customers).sum();
+
+		int[] result = new int[n];
+
+		for (int i = 0; i < customers.length; i++) {
+			result[0] += customers[i];
+			Arrays.sort(result);
+		}
+
+		return result[n - 1];
+	}
+}
